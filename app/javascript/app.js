ca4f82b57f4c5c216a28f85ae71ddceec8029812
茶木 涼 <>
2022-07-11 14:08:44 +0900

    popper諦めて手動でヘッダートグル

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,5 +1,20 @@
-$(function(){
+$('turbolinks:render',function(){
   $('h1').css('color',"#333");
   console.log('jqueryは読み込んでる');
+
+  $('.dropdown-toggle').on('click',(e)=>{
+    e.preventDefault();
+
+    $('.dropdown-menu').toggle();
+  });
+  
+  $(document).on('click',function(e) {
+    if(!$(e.target).closest('.dropdown-toggle').length) {
+      $('.dropdown-menu').hide();
+    } else {
+
+    }
+ });
+
   
 })
