06a1ca26b69ad4ea6f3fa30d455df8de4ac52734
茶木 涼 <>
2022-08-17 11:08:42 +0900

    no-message

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,8 @@
-Aws.config.update({
-    region: 'ap-northeast-1',
-    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-})
+# Aws.config.update({
+#     region: 'ap-northeast-1',
+#     credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+# })
 
 
-puts ENV['AWS_ACCESS_KEY_ID']
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['BUCKET'])
+# puts ENV['AWS_ACCESS_KEY_ID']
+# S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['BUCKET'])
