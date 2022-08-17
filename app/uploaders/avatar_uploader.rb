e47a428a877184dd858edbd9792eb33c5c900936
茶木 涼 <>
2022-08-17 13:28:34 +0900

    modelid

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -42,6 +42,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "avatar-#{@current_user.id}.#{file.extension}" if original_filename.present?
+    "avatar-#{model.id}.#{file.extension}" if original_filename.present?
   end
 end
