54fe68d26dbf9d5f7a9c1414aafa125f32ffbd64
chaki <aowp.contact@gmail.com>
2022-08-27 10:46:43 +0900

    完成

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,6 @@
 class Word < ApplicationRecord
   belongs_to :user
 
-  validates :term, presence: true, length: { minimum: 2 }, uniqueness: true
+  # validates :term,  
   validates :conversion, presence: true
 end
